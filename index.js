ba82bf70c6e719b6b39774a6d5dfdddcf2f0efe0
Fabricio Godoy <firgodoy@hotmail.com>
2022-12-22 22:27:25 -0300

    Segunda entrega

--- index.js
+++ index.js
@@ -1,39 +1,68 @@
+const fs = require('fs');
+
 class ProductManager {
-    static contadorId = 0;
-
-    constructor(title,description,price,thumbnail,code,stock) {
-        this.products = [];
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-    }
-    addProduct = () => {
-        this.products.push({title,description,price,thumbnail,code,stock})
-        if (title === "" && description === "" && price === "" && thumbnail === "" && code === "" && stock === "") {
-            console.log("Complete todos los campos por favor")
-            return false;
-
-        }
-        const listado = [...new Set(this.products)];
-        ProductManager.contadorId++;
+    
+    constructor(path) {
+        this.products = cargaProduct();
+        this.id = 0;
+        this.path = path;
         
+
+
     }
-    getProducts = () => {
+    addProduct(title, description, price, thumbnail, code, stock) {
+
+        if (!title && !description && !price && !thumbnail && !code && !stock) {
+            return "Complete todos los campos por favor"
+
+        } else {
+            this.products.push({ title, description, price, thumbnail, code, stock, id: this.id++ })
+            const listado = [...new Set(this.products)];
+            return listado;  
+        }
+      this.guardaProduct();
+    }
+
+    getProducts() {
         return this.products;
     }
-    getProductById = () => {
-        if (id === id) {
-            return id
-        }
-    }; else() {
-        console.log("Not found")
-    }
-}
-const producto1 = new ProductManager ("Id:1", "Motorola G71", "Colores mas vibrantes, full HD+, OLED, 6.4 pulgadas, DCI-P3, rango de colores 25% mas amplio","$60.999","https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/g/7/g71-verde-frente.png","G71","50");
-const producto2 = new ProductManager ("Id:2", "Motorola G22", "Colores mas vibrantes, full HD+, OLED, 6.4 pulgadas, DCI-P3, rango de colores 25% mas amplio","$59.999","https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/g/7/g71-verde-frente.png","G22","30");
-const producto3 = new ProductManager ("Id:3", "Motorola E40", "Colores mas vibrantes, full HD+, OLED, 6.4 pulgadas, DCI-P3, rango de colores 25% mas amplio","$52.999","https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/g/7/g71-verde-frente.png","E40","10");
+    getProductById(id) {
 
-console.log (this.products)
+        const IdProducto = this.products.find(prod => prod.id === id)
+
+        if (IdProducto) {
+            return IdProducto
+        } else {
+            return "Not found"
+        }
+
+    }
+    cargaProduct() {
+        try {
+            this.products = JSON.parse (fs.readFileSync(this.path, "utf-8"))
+        }catch(err) {
+            throw new Error (err);
+        }
+
+    }
+    guardaProduct() {
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
+        }catch(err) {
+            throw new Error (err);
+        }
+     }
+     eliminaProduct(){
+        try {
+            fs.unlinkSync(this.path);
+        }catch(err){
+            throw new Error (err);
+        }
+     }
+}
+
+productManager = new ProductManager('./productos.json');
+
+
+
+
